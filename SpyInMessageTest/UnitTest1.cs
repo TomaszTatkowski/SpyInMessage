eb904e9e655666717ee5d4825ba221e66a388d2e
Tomasz <tomasz.tatkowski@gmail.com>
2019-02-02 12:15:13 +0000

    add unit test

--- SpyInMessageTest/UnitTest1.cs
+++ SpyInMessageTest/UnitTest1.cs
@@ -23,10 +23,12 @@ namespace SpyInMessageTest
             SpyInMessage spyInMessage = new SpyInMessage();
             int[] message = new int[] {1, 2, 3, 4, 5, 6};
             int[] spyTrue = new int[] { 1, 3, 6 };
-            int[] spyFalse = new int[] { 3, 2, 6 };
+            int[] spyFalse1 = new int[] { 3, 2, 6};
+            int[] spyFalse2 = new int[] { 3, 2, 6, 7};
 
             Assert.IsTrue(spyInMessage.isSpyInMessage(message, spyTrue));
-            Assert.IsFalse(spyInMessage.isSpyInMessage(message, spyFalse));
+            Assert.IsFalse(spyInMessage.isSpyInMessage(message, spyFalse1));
+            Assert.IsFalse(spyInMessage.isSpyInMessage(message, spyFalse2));
         }
 
 
